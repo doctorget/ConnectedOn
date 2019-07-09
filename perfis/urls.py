cf3a838efe40655d862ebc30df2f6f1bbc5a5043
Samuel <samuelsoares177778@gmail.com>
2019-07-09 10:05:54 -0300

    recomeçando projeto com django 1.7.4

--- perfis/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, re_path
-from perfis import views
-
-urlpatterns = [
-    path(r'', views.index, name='index'),
-    path(r'perfis/', views.exibir),
-    path(r'<int:perfil_id>/', views.exibir)
-]
