d7b3b41c49d1c621a234cb2738ee91edaa159bd2
Samuel <samuelsoares177778@gmail.com>
2019-08-02 21:12:02 -0300

    arrumando extensões

--- perfis/views.py
+++ perfis/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from perfis.models import Perfil
 
 def index(request):
-    return render(request, 'index.html', {'perfis' : Perfil.objets.all()})
+    return render(request, 'index.html', {'perfis' : Perfil.objects.all()})
 
 def exibir(request):
     return render(request, 'perfil.html')
